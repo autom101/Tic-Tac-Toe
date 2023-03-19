37b030b0bc255c79a96c8ea187f3b3cd387ab7bc
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-03-18 19:00:10 -0600

    Add functions to change score on webpage

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 let container = document.querySelector(".tic-tac-toe-container");
 let playButton = document.querySelector(".play-button");
 
+let playerOneWin = document.querySelector(".player-one-wins");
+let playerOneLoss = document.querySelector(".player-one-losses");
+let playerOneTie = document.querySelector(".player-one-ties");
+
+let playerTwoWin = document.querySelector(".player-two-wins");
+let playerTwoLoss = document.querySelector(".player-two-losses");
+let playerTwoTie = document.querySelector(".player-two-ties");
+
+const Player = () => {
+  let wins = 0;
+  let losses = 0;
+  let ties = 0;
+  return { wins, losses, ties };
+};
+
 const ticTacToeGame = (() => {
   let placedO = true;
   let numberOfTurns = 0;
   let winner = false;
   let tie = false;
   let gameBoard = [[], [], []];
+  let player1 = Player();
+  let player2 = Player();
 
   // Resets everything and starts a new game
   const newGame = () => {
@@ -17,7 +34,7 @@ const ticTacToeGame = (() => {
     gameBoard = [[], [], []];
     let boardInDom = [[], [], []];
 
-    // Continously deleted the first Child of container until it has no children left
+    // Continously delete the first Child of container until it has no children left
     while (container.firstChild) {
       container.removeChild(container.firstChild);
     }
@@ -57,20 +74,22 @@ const ticTacToeGame = (() => {
         board[i][j].addEventListener(
           "click",
           () => {
-            numberOfTurns++;
+            if (!winner) {
+              numberOfTurns++;
 
-            if (placedO) {
-              board[i][j].classList.toggle("add-X");
-              gameBoard[i][j]++;
-              checkState(gameBoard);
+              if (placedO) {
+                board[i][j].classList.toggle("add-X");
+                gameBoard[i][j]++;
+                checkState(gameBoard);
 
-              placedO = false;
-            } else {
-              board[i][j].classList.toggle("add-O");
-              gameBoard[i][j]--;
-              checkState(gameBoard);
+                placedO = false;
+              } else {
+                board[i][j].classList.toggle("add-O");
+                gameBoard[i][j]--;
+                checkState(gameBoard);
 
-              placedO = true;
+                placedO = true;
+              }
             }
           },
           { once: true }
@@ -80,85 +99,121 @@ const ticTacToeGame = (() => {
     return board;
   };
 
+  // Show new player score on DOM
+  const displayPlayerScore = () => {
+    //
+    playerOneWin.innerHTML = player1.wins;
+    playerOneLoss.innerHTML = player1.losses;
+    playerOneTie.innerHTML = player1.ties;
+
+    playerTwoWin.innerHTML = player2.wins;
+    playerTwoLoss.innerHTML = player2.losses;
+    playerTwoTie.innerHTML = player2.ties;
+  };
+
+  // Update player objects when there is a win/loss/tie
+  const updatePlayerScore = (whoWon = "tie") => {
+    if (whoWon === "playerOne") {
+      player1.wins += 1;
+      player2.losses += 1;
+    } else if (whoWon == "playerTwo") {
+      player2.wins += 1;
+      player1.losses += 1;
+    } else {
+      player1.ties += 1;
+      player2.ties += 1;
+    }
+
+    displayPlayerScore();
+  };
+
   // Checks if any win conditions have been met:
   const checkState = (board) => {
     //Check row0
     if (numberOfTurns >= 4 && winner === false) {
-      if (
-        board[0][0] + board[0][1] + board[0][2] === 3 ||
-        board[0][0] + board[0][1] + board[0][2] === -3
-      ) {
-        console.log("Winnnerrrrr row0");
-        winner = true;
+      if (board[0][0] + board[0][1] + board[0][2] === 3) {
+        updatePlayerScore("playerOne");
+        return;
+      }
+      if (board[0][0] + board[0][1] + board[0][2] === -3) {
+        updatePlayerScore("playerTwo");
+        return;
       }
 
       //Check row1
-      if (
-        board[1][0] + board[1][1] + board[1][2] === 3 ||
-        board[1][0] + board[1][1] + board[1][2] === -3
-      ) {
-        console.log("Winnnerrrrr row1");
-        winner = true;
+      if (board[1][0] + board[1][1] + board[1][2] === 3) {
+        updatePlayerScore("playerOne");
+        return;
+      }
+      if (board[1][0] + board[1][1] + board[1][2] === -3) {
+        updatePlayerScore("playerTwo");
+        return;
       }
 
       //Check row2
-      if (
-        board[2][0] + board[2][1] + board[2][2] === 3 ||
-        board[2][0] + board[2][1] + board[2][2] === -3
-      ) {
-        console.log("Winnnerrrrr row2");
-        winner = true;
+      if (board[2][0] + board[2][1] + board[2][2] === 3) {
+        updatePlayerScore("playerOne");
+        return;
+      }
+      if (board[2][0] + board[2][1] + board[2][2] === -3) {
+        updatePlayerScore("playerTwo");
+        return;
       }
 
       //Check col0
-      if (
-        board[0][0] + board[1][0] + board[2][0] === 3 ||
-        board[0][0] + board[1][0] + board[2][0] === -3
-      ) {
-        console.log("Winnnerrrrr col0");
-        winner = true;
+      if (board[0][0] + board[1][0] + board[2][0] === 3) {
+        updatePlayerScore("playerOne");
+        return;
+      }
+      if (board[0][0] + board[1][0] + board[2][0] === -3) {
+        updatePlayerScore("playerTwo");
+        return;
       }
 
       //Check col1
-      if (
-        board[0][1] + board[1][1] + board[2][1] === 3 ||
-        board[0][1] + board[1][1] + board[2][1] === -3
-      ) {
-        console.log("Winnnerrrrr col1");
-        winner = true;
+      if (board[0][1] + board[1][1] + board[2][1] === 3) {
+        updatePlayerScore("playerOne");
+        return;
+      }
+      if (board[0][1] + board[1][1] + board[2][1] === -3) {
+        updatePlayerScore("playerTwo");
+        return;
       }
 
       //Check col2
-      if (
-        board[0][2] + board[1][2] + board[2][2] === 3 ||
-        board[0][2] + board[1][2] + board[2][2] === -3
-      ) {
-        console.log("Winnnerrrrr col2");
-        winner = true;
+      if (board[0][2] + board[1][2] + board[2][2] === 3) {
+        updatePlayerScore("playerOne");
+        return;
+      }
+      if (board[0][2] + board[1][2] + board[2][2] === -3) {
+        updatePlayerScore("playerTwo");
+        return;
       }
 
       //Check forwards-diagonal
-      if (
-        board[0][0] + board[1][1] + board[2][2] === 3 ||
-        board[0][0] + board[1][1] + board[2][2] === -3
-      ) {
-        console.log("Winnnerrrrr forward-diagonal");
-        winner = true;
+      if (board[0][0] + board[1][1] + board[2][2] === 3) {
+        updatePlayerScore("playerOne");
+        return;
+      }
+      if (board[0][0] + board[1][1] + board[2][2] === -3) {
+        updatePlayerScore("playerTwo");
+        return;
       }
 
       //Check backwards-diagonal
-      if (
-        board[0][2] + board[1][1] + board[2][0] === 3 ||
-        board[0][2] + board[1][1] + board[2][0] === -3
-      ) {
-        console.log("Winnnerrrrr backward-diagonal");
-        winner = true;
+      if (board[0][2] + board[1][1] + board[2][0] === 3) {
+        updatePlayerScore("playerOne");
+        return;
+      }
+      if (board[0][2] + board[1][1] + board[2][0] === -3) {
+        updatePlayerScore("playerTwo");
+        return;
       }
     }
 
     if (numberOfTurns === 9 && winner === false) {
-      console.log("tie :(");
-      tie = true;
+      updatePlayerScore("tie");
+      return;
     }
   };
 
